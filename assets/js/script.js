0a998a35496de7ef04a685e791a678838c9b2dd7
Sean Worrell <spworrell@gmail.com>
2023-08-01 16:57:06 -0400

    BCS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
+
+
 var searchCount = 0
+var dayURL = "api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=bf922896d93871f3fce26701fa6fe44c&units=imperial"
+var locURL = "http://api.openweathermap.org/geo/1.0/direct?q=Orlando&limit=5&appid=bf922896d93871f3fce26701fa6fe44c"
 var loc = ""
 var lat = ""
 var lon = ""
@@ -13,7 +17,7 @@ searchCount--
 }
 
 if (searchCount <= 4) {
-var loc = $(`#cityInp`).val()
+loc = $(`#cityInp`).val()
 $(`#cities`).prepend(`<button class="btn btn-primary active searched" role="button" style ="width: 100%"  aria-pressed="true">${loc}</button>`)
 searchCount++
 
@@ -26,9 +30,37 @@ searchCount++
 
 // Location Search
 
+function locationSearch()
+{
+    
+    fetch(locURL, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+    
+      });
+    
+    console.log(loc)
+    console.log(data)
+        
+    
+   
 
+
+}
 
 
 // Weather Search
 
+// api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=bf922896d93871f3fce26701fa6fe44c&units=imperial
 
+// http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bf922896d93871f3fce26701fa6fe44c
+
+// function getReport() {
+
+
+// }
